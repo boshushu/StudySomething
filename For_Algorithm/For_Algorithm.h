eee926af4bd5344d600561ea1830887f51f647e6
boshushu <1139643794@qq.com>
2020-04-16 17:17:13 +0800

    新增 Sunday算法实现

--- For_Algorithm/For_Algorithm.h
+++ For_Algorithm/For_Algorithm.h
@@ -5,7 +5,7 @@ using namespace std;
 
 
 /*
-File: BitCounr.cpp
+File: BitCount.cpp
 Func: 计算整数里的1的个数
 */
 unsigned int BitCount(unsigned int n);
@@ -18,3 +18,6 @@ File: KMP.cpp
 Func: KMP算法实现
 */
 int KMP(char *p_Text, char *p_Pattern);
+int BMSearch(char *p_Str, char *p_Pat);
+int SundaySearch(char *p_Str, char *p_Pat);
+
--- For_Algorithm/KMP.cpp
+++ For_Algorithm/KMP.cpp
@@ -1,6 +1,9 @@
 #include "For_Algorithm.h"
 
 
+/*
+KMP算法
+*/
 int KMP(char *p_Text, char *p_Pattern)
 {
 	bool bIsPat = false;
@@ -40,6 +43,57 @@ int KMP(char *p_Text, char *p_Pattern)
 				}
 			}
 		}
+	}
+	return -1;
+}
+
+
+
+/*
+BM算法
+*/
+int BMSearch(char *p_Str, char *p_Pat)
+{
+	return 0;
+}
+
+
+
+/*
+Sunday算法
+从前往后匹配，在匹配失败时关注的是文本串中参加匹配的最末位字符的下一位字符。
+ */
+int IsBitInStr(char a, char* b)
+{
+	for (int i = 0; i < strlen(b); i++)
+		if (a == b[i])
+			return i;
+	return -1;
+}
+
+int SundaySearch(char *p_Str, char *p_Pat)
+{
+	int iIndex = 0, iLength = strlen(p_Pat);
+	bool bIsPat = false;
+	while (iIndex < (strlen(p_Str) - iLength))
+	{
+		for (int j = 0; j < iLength; j++)
+		{
+			if (p_Str[j + iIndex] == p_Pat[j])
+				bIsPat = true;
+			else
+			{
+				//1.如果该字符没有在模式串中出现则直接跳过，即移动位数 = 匹配串长度 + 1;
+				//2.否则，其移动位数 = 模式串中最右端的该字符到末尾的距离+1;
+				int tmpIndex = IsBitInStr(p_Str[iIndex + iLength], p_Pat);
+				iIndex += ((-1 == tmpIndex) ? (iLength + 1) : tmpIndex);
+				bIsPat = false;
+				break;
+			}
+		}
+
+		if (bIsPat == true)
+			return iIndex;
 	}
 	return -1;
 }
